9ea8c0e2cc84f101db8b825ff383b7b8179a725d
Bengisu Güler <47686770+bengisuglr@users.noreply.github.com>
2019-03-13 22:13:32 +0300

    Delete lol.py

--- lol.py
+++
@@ -1,5 +0,0 @@
-lover = input( "what is the name of your bf?" )
-age = int ( input( "how old is", lover, "?"))
-ageyou = int ( input ( "how old are you?" ))
-x = age - ageyou
-print( "acceptable if 0 <", x )
